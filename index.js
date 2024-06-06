6608bb2853f24f45e2268b40c3102f6d539ce01c
shantanusawant26 <shantanusawant26@gmail.com>
2024-06-06 15:38:29 +0530

    chages

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+
+
+
+
+app.use("/",(req, res) => {
+    res.json("Both userMessage and response fields are required" );
+})
+
+const PORT = 3006;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`); 
+});
+
+
+
+
