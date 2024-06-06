a7cc016030d64d3c2811c3cce080b10ca29fb3ff
shantanusawant26 <shantanusawant26@gmail.com>
2024-06-06 16:28:09 +0530

    changes

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const conatactUsRouters = require("./contactUsRouters");
 app.use("/",(req, res) => {
     res.json("Both userMessage and response fields are required" );
 })
-app.use("/contact", conatactUsRouters);
+app.use("/contact", (req, res) => {
+    res.json("B" );
+});
 const PORT = 3006;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`); 
