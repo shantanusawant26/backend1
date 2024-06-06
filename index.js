a4e5d72c5c045f86f66c5e1b1b0e9b3e21866a1d
shantanusawant26 <shantanusawant26@gmail.com>
2024-06-06 15:57:13 +0530

    chages

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const conatactUsRouters = require("./contactUsRouters");
 app.use("/",(req, res) => {
     res.json("Both userMessage and response fields are required" );
 })
-app.use("/contact", conatactUsRouters);
+// app.use("/contact", conatactUsRouters);
 const PORT = 3006;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`); 
