3c2343197e1b85e8ef3b650100172e11b2953415
shantanusawant26 <shantanusawant26@gmail.com>
2024-06-06 16:01:26 +0530

    chages

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const app = express();
 
 // const dbconnection= require("./db")
 
-
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://shantanusawant26:RgQ7INcGijt5L7iM@cluster0.pbuzob9.mongodb.net/vsTechHorizon?retryWrites=true&w=majority";
+//const uri ="mongodb+srv://vstechhorizon:Software@cluster0.kljlv7f.mongodb.net/vsTechHorizon?retryWrites=true&w=majority&appName=Cluster0";
+// Connect to MongoDB Atlas
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB Atlas');
+        // If connected successfully, proceed to save data
+        // saveData();
+    })
+    .catch(err => console.error('Failed to connect to MongoDB Atlas:', err));
 
 
 
