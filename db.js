d788e4715363e25ec68daf3163a2879634e78dcf
shantanusawant26 <112574366+shantanusawant26@users.noreply.github.com>
2024-06-07 16:51:07 +0530

    Update db.js
    added

--- db.js
+++ db.js
@@ -44,7 +44,7 @@ const mongoose = require('mongoose');
 const uri = "mongodb+srv://shantanusawant26:RgQ7INcGijt5L7iM@cluster0.pbuzob9.mongodb.net/vsTechHorizon?retryWrites=true&w=majority";
 //const uri ="mongodb+srv://vstechhorizon:Software@cluster0.kljlv7f.mongodb.net/vsTechHorizon?retryWrites=true&w=majority&appName=Cluster0";
 // Connect to MongoDB Atlas
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(uri)
     .then(() => {
         console.log('Connected to MongoDB Atlas');
         // If connected successfully, proceed to save data
